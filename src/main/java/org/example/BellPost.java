23b410093982afbbb2b99f93a3ea3f244d513873
neerajmalhotra <neeraj@bellpost.com>
2024-02-01 12:25:02 +0530

    refactored code

--- src/main/java/org/example/BellPost.java
+++ src/main/java/org/example/BellPost.java
@@ -10,7 +10,7 @@ public class BellPost {
         driver.manage().window().maximize();
         driver.get("https://retaildev.bellpost.com/#/app");
 
-        Thread.sleep(2000);
+        Thread.sleep(3000);
         driver.close();
 
     }
