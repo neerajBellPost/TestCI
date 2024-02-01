e1aff997a61d084a1ba02553f24ffd6defe91443
neerajmalhotra <neeraj@bellpost.com>
2024-02-01 14:01:31 +0530

    refactored code

--- src/main/java/org/example/BellPost.java
+++ src/main/java/org/example/BellPost.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,6 +10,9 @@ public class BellPost {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://retaildev.bellpost.com/#/app");
+
+//        driver.findElement(By.xpath("/html/body/flutter-view/flt-text-editing-host/input")).
+//                sendKeys("testing123");
 
         Thread.sleep(4000);
         driver.close();
